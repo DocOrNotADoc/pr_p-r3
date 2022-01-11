73121eacaa0a850ad0fa6d5ac30426142fdca469
Malgin Viktor <leesunsinn@protonmail.com>
2022-01-12 00:55:39 +0300

    clickable words & some re-factoring

--- src/js/script.js
+++ src/js/script.js
@@ -4,141 +4,142 @@ const statesArray = [
     word: 'start',
     presentThird: 'starts',
     past: 'started',
-    classList: '',
+    classes: '',
     translateRus: 'начать'
   },
   {
     word: 'love',
     presentThird: 'loves',
     past: 'loved',
-    classList: '',
+    classes: '',
     translateRus: 'любить'
   },
   {
     word: 'have',
     presentThird: 'has',
     past: 'had',
-    classList: 'red',
+    classes: 'red',
     translateRus: 'иметь'
   },
   {
     word: 'do',
     presentThird: 'does',
     past: 'did',
-    classList: 'red',
+    classes: 'red',
     translateRus: 'делать'
   },
   {
     word: 'say',
     presentThird: 'says',
     past: 'sayd',
-    classList: 'red',
+    classes: 'red',
     translateRus: 'говорить'
   },
   {
     word: 'go',
     presentThird: 'goes',
     past: 'went',
-    classList: 'red',
+    classes: 'red',
     translateRus: 'идти'
   },
   {
     word: 'get',
     presentThird: 'gets',
     past: 'got',
-    classList: 'red',
+    classes: 'red',
     translateRus: 'получать'
   },
-  // {
-  //   word: 'make',
-  //   presentThird: 'makes',
-  //   past: 'made',
-  //   classList: 'red',
-  //   translateRus: 'сделать'
-  // },
-  // чтобы было i = 1-10
+  {
+    word: 'make',
+    presentThird: 'makes',
+    past: 'made',
+    classes: 'red',
+    translateRus: 'сделать'
+  },
   {
     word: 'know',
     presentThird: 'knows',
     past: 'knew',
-    classList: 'red',
+    classes: 'red',
     translateRus: 'знать'
   },
   {
     word: 'think',
     presentThird: 'thinks',
     past: 'thought',
-    classList: 'red',
+    classes: 'red',
     translateRus: 'думать'
   },
   {
     word: 'take',
     presentThird: 'takes',
     past: 'took',
-    classList: 'red',
+    classes: 'red',
     translateRus: 'брать'
   }
 ]
 // red - неправильный глагол
-
-updateWordBtn.addEventListener('click', (idx = 0) => {
+updateWord = () => {
   const futureQ = document.querySelector("#futureQ"),
-    futureA = document.querySelector("#futureA"),
-    futureN = document.querySelector("#futureN"),
-    presentQ = document.querySelector("#presentQ"),
-    presentAFirst = document.querySelector("#presentAFirst"),
-    presentAThird = document.querySelector("#presentAThird"),
-    presentN = document.querySelector("#presentN"),
-    pastQ = document.querySelector("#pastQ"),
-    pastA = document.querySelector("#pastA"),
-    pastN = document.querySelector("#pastN"),
-    idx = Math.floor(Math.random() * 11);
+  futureA = document.querySelector("#futureA"),
+  futureN = document.querySelector("#futureN"),
+  presentQ = document.querySelector("#presentQ"),
+  presentAFirst = document.querySelector("#presentAFirst"),
+  presentAThird = document.querySelector("#presentAThird"),
+  presentN = document.querySelector("#presentN"),
+  pastQ = document.querySelector("#pastQ"),
+  pastA = document.querySelector("#pastA"),
+  pastN = document.querySelector("#pastN"),
+  idx = Math.floor(Math.random() * 11),
+  state = statesArray[idx],
+  irregularVerbsMainInTable = [futureQ, futureA, futureN, presentQ, presentAFirst, presentN],
+  regularVerbsMainInTable = [...irregularVerbsMainInTable, pastQ, pastN],
+  // allVerbsThirdInTable = [presentAThird],
+  // regularVerbsPastInTable = [pastA],
+  irregularVerbsPastInTable = [pastQ, pastA, pastN],
+  irregularVerbsPaint = [...irregularVerbsMainInTable, presentAThird, ...irregularVerbsPastInTable];
 
-    
-    // state.presentThird => чтобы манипулировать, надо перевести в класс. Затем:
-    // if (state.word.substr(-1) !== 'e') {
-    //   state.presentThird = `${testWord}es`;
-    // }
-    // state.presentThird = `${testWord}s`;
-    // 
-    // if (state.classList === '') {
-    //   if (state.word.substr(-1) !== 'e') {
-    //     state.past = `${testWord}ed`;
-    //   }
-    //   state.past = `${testWord}d`;
-    // }
-      
-  const state = statesArray[idx];
+  paintWord = () => {
+    irregularVerbsPaint.forEach(item => item.className = `clickable ${state.classes}`);
+  }
 
+  // state.presentThird => чтобы манипулировать, надо перевести в класс. Затем:
+  // if (state.word.substr(-1) !== 'e') {
+  //   state.presentThird = `${testWord}es`;
+  // }
+  // if (state.word.substr(-1) === 'e') {
+  //   state.presentThird = `${testWord}s`;
+  // }
+  // 
+  // if (state.classes === '') {
+  //   if (state.word.substr(-1) !== 'e') {
+  //     state.past = `${testWord}ed`;
+  //   }
+  //   if (state.word.substr(-1) === 'e') {
+  //     state.past = `${testWord}d`;
+  //   }
+  // }
 
-  const irregularVerbsMainInTable = [futureQ, futureA, futureN, presentQ, presentAFirst, presentN],
-    regularVerbsMainInTable = [...irregularVerbsMainInTable, pastQ, pastN],
-    // allVerbsThirdInTable = [presentAThird],
-    // regularVerbsPastInTable = [pastA],
-    irregularVerbsPastInTable = [pastQ, pastA, pastN],
-    irregularVerbsPaint = [...irregularVerbsMainInTable, presentAThird, ...irregularVerbsPastInTable];
-      
-    paintWord = () => {
-      irregularVerbsPaint.forEach(item => item.className = state.classList);
-    }
-
-    if (state.classList === 'red') {
+  switch (state.classes) {
+    case  '':
       irregularVerbsMainInTable.forEach(item => item.textContent = state.word);
       presentAThird.textContent = state.presentThird;
       irregularVerbsPastInTable.forEach(item => item.textContent = state.past);
       paintWord();
-    }
-
-    if (state.classList === '') {
+      break;
+    case 'red':
       regularVerbsMainInTable.forEach(item => item.textContent = state.word);
       presentAThird.textContent = state.presentThird;
       pastA.textContent = state.past;
       paintWord();
-    }
+      break;
+    default:
+      console.log("Ooops...");
+  }
+}
+updateWord();
 
+updateWordBtn.addEventListener('click', (e) => updateWord());
 
-  // 1. Проверить, надо ли зачёркивать слово перед указанием кто, если прошедшее время вопр. Форма. И надо ли заменять did на do в этом случае?(негатив)
-  // 2. Сделать стейт классом. Это оптимизирует код. И менять содержимое, беря из псевдо-АПИ.
-
-  
-})
+// 4. Сделать по клику на глагол всплывающее мини окошко с переводом .popup
+// 5. При клике вне окошка и на крестик закрывать его
