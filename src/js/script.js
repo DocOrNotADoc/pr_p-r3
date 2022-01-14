ffb4f929a81f62adcf966bf636edeec977ab9490
Malgin Viktor <leesunsinn@protonmail.com>
2022-01-15 00:09:10 +0300

    made present-third verbs switch & correct auto-render

--- src/js/script.js
+++ src/js/script.js
@@ -4,14 +4,14 @@ const statesArray = [
 // classes: 'red' - неправильный глагол
   {
     word: 'start',
-    presentThird: 'starts',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'начать'
   },
   {
     word: 'love',
-    presentThird: 'loves',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'любить'
@@ -32,7 +32,7 @@ const statesArray = [
   },
   {
     word: 'say',
-    presentThird: 'says',
+    presentThird: '',
     past: 'said',
     classes: 'red',
     translateRus: 'говорить'
@@ -46,28 +46,28 @@ const statesArray = [
   },
   {
     word: 'get',
-    presentThird: 'gets',
+    presentThird: '',
     past: 'got',
     classes: 'red',
     translateRus: 'получать'
   },
   {
     word: 'make',
-    presentThird: 'makes',
+    presentThird: '',
     past: 'made',
     classes: 'red',
     translateRus: 'сделать'
   },
   {
     word: 'know',
-    presentThird: 'knows',
+    presentThird: '',
     past: 'knew',
     classes: 'red',
     translateRus: 'знать'
   },
   {
     word: 'think',
-    presentThird: 'thinks',
+    presentThird: '',
     past: 'thought',
     classes: 'red',
     translateRus: 'думать'
@@ -75,42 +75,42 @@ const statesArray = [
   //10
   {
     word: 'take',
-    presentThird: 'takes',
+    presentThird: '',
     past: 'took',
     classes: 'red',
     translateRus: 'брать'
   },
   {
     word: 'see',
-    presentThird: 'sees',
+    presentThird: '',
     past: 'saw',
     classes: 'red',
     translateRus: 'видеть'
   },
   {
     word: 'come',
-    presentThird: 'comes',
+    presentThird: '',
     past: 'came',
     classes: 'red',
     translateRus: 'приходить'
   },
   {
     word: 'want',
-    presentThird: 'wants',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'хотеть'
   },
   {
     word: 'look',
-    presentThird: 'looks',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'смотреть'
   },
   {
     word: 'use',
-    presentThird: 'uses',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'использовать'
@@ -124,21 +124,21 @@ const statesArray = [
   },
   {
     word: 'give',
-    presentThird: 'gives',
+    presentThird: '',
     past: 'gave ',
     classes: 'red',
     translateRus: 'давать'
   },
   {
     word: 'tell',
-    presentThird: 'tells',
+    presentThird: '',
     past: 'told ',
     classes: 'red',
     translateRus: 'рассказать'
   },
   {
     word: 'work',
-    presentThird: 'works',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'работать'
@@ -146,7 +146,7 @@ const statesArray = [
   // 20
   {
     word: 'call',
-    presentThird: 'calls',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'звонить, называть, вызывать, звать, призывать, звонить по телефону, обзывать, окликать, кликать, считать, созывать, заходить'
@@ -167,49 +167,49 @@ const statesArray = [
   },
   {
     word: 'need',
-    presentThird: 'need',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'нуждаться'
   },
   {
     word: 'feel',
-    presentThird: 'feel',
+    presentThird: '',
     past: 'felt',
     classes: 'red',
     translateRus: 'чувствовать'
   },
   {
     word: 'become',
-    presentThird: 'becomes',
+    presentThird: '',
     past: 'became',
     classes: 'red',
     translateRus: 'становиться'
   },
   {
     word: 'leave',
-    presentThird: 'leaves',
+    presentThird: '',
     past: 'left',
     classes: 'red',
     translateRus: 'уходить'
   },
   {
     word: 'put',
-    presentThird: 'puts',
+    presentThird: '',
     past: 'put',
     classes: 'red',
     translateRus: 'положить'
   },
   {
     word: 'mean',
-    presentThird: 'means',
+    presentThird: '',
     past: 'meant',
     classes: 'red',
     translateRus: 'иметь ввиду'
   },
   {
     word: 'keep',
-    presentThird: 'keeps',
+    presentThird: '',
     past: 'kept',
     classes: 'red',
     translateRus: 'держать'
@@ -217,70 +217,70 @@ const statesArray = [
   // 30
   {
     word: 'let',
-    presentThird: 'lets',
+    presentThird: '',
     past: 'let',
     classes: 'red',
     translateRus: 'позволять'
   },
   {
     word: 'begin',
-    presentThird: 'begins',
+    presentThird: '',
     past: 'began',
     classes: 'red',
     translateRus: 'начать'
   },
   {
     word: 'seem',
-    presentThird: 'seems',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'выглядеть, казаться'
   },
   {
     word: 'help',
-    presentThird: 'helps',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'помогать'
   },
   {
     word: 'talk',
-    presentThird: 'talks',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'говорить'
   },
   {
     word: 'turn',
-    presentThird: 'turns',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'поворачиваться'
   },
   {
     word: 'show',
-    presentThird: 'shows',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'показывать'
   },
   {
     word: 'hear',
-    presentThird: 'hears',
+    presentThird: '',
     past: 'heard',
     classes: 'red',
     translateRus: 'услышать'
   },
   {
     word: 'play',
-    presentThird: 'plays',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'играть'
   },
   {
     word: 'run',
-    presentThird: 'runs',
+    presentThird: '',
     past: 'ran',
     classes: 'red',
     translateRus: 'бежать'
@@ -288,70 +288,70 @@ const statesArray = [
   // 40
   {
     word: 'move',
-    presentThird: 'moves',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'двигаться'
   },
   {
     word: 'like',
-    presentThird: 'likes',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'нравиться'
   },
   {
     word: 'live',
-    presentThird: 'lives',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'жить'
   },
   {
     word: 'believe',
-    presentThird: 'believes',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'верить'
   },
   {
     word: 'hold',
-    presentThird: 'holds',
+    presentThird: '',
     past: 'held',
     classes: 'red',
     translateRus: 'держать'
   },
   {
     word: 'bring',
-    presentThird: 'brings',
+    presentThird: '',
     past: 'brought',
     classes: 'red',
     translateRus: 'приносить'
   },
   {
     word: 'happen',
-    presentThird: 'happens',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'происходить, оказываться'
   },
   {
     word: 'write',
-    presentThird: 'writes',
+    presentThird: '',
     past: 'wrote',
     classes: 'red',
     translateRus: 'писать'
   },
   {
     word: 'provide',
-    presentThird: 'provides',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'снабжать, предусматривать, предоставлять'
   },
   {
     word: 'sit',
-    presentThird: 'sits',
+    presentThird: '',
     past: 'sat',
     classes: 'red',
     translateRus: 'сидеть'
@@ -359,70 +359,70 @@ const statesArray = [
   // 50
   {
     word: 'stand',
-    presentThird: 'stands',
+    presentThird: '',
     past: 'stood',
     classes: 'red',
     translateRus: 'стоять'
   },
   {
     word: 'lose',
-    presentThird: 'loses',
+    presentThird: '',
     past: 'lost',
     classes: 'red',
     translateRus: 'терять'
   },
   {
     word: 'pay',
-    presentThird: 'pays',
+    presentThird: '',
     past: 'paid',
     classes: 'red',
     translateRus: 'платить'
   },
   {
     word: 'meet',
-    presentThird: 'meets',
+    presentThird: '',
     past: 'met',
     classes: 'red',
     translateRus: 'встречать'
   },
   {
     word: 'include',
-    presentThird: 'includes',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'включать(в смысле принадлежности к, например, общему количеству)'
   },
   {
     word: 'continue',
-    presentThird: 'continues',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'продолжать'
   },
   {
     word: 'set',
-    presentThird: 'sets',
+    presentThird: '',
     past: 'set',
     classes: 'red',
     translateRus: 'устанавливать'
   },
   {
     word: 'learn',
-    presentThird: 'learns',
+    presentThird: '',
     past: 'learnt / learned',
     classes: 'red',
     translateRus: 'учиться'
   },
   {
     word: 'change',
-    presentThird: 'changes',
+    presentThird: '',
     past: '',
     classes: '',
     translateRus: 'менять'
   },
   {
     word: 'lead',
-    presentThird: 'leads',
+    presentThird: '',
     past: 'led',
     classes: 'red',
     translateRus: 'вести'
@@ -430,7 +430,7 @@ const statesArray = [
   // 60
   {
     word: 'lead',
-    presentThird: 'leads',
+    presentThird: '',
     past: 'led',
     classes: 'red',
     translateRus: 'вести'
@@ -452,7 +452,7 @@ updateWord = () => {
   popupShow = document.querySelector(".popup-fill"),
   popupText = document.querySelector(".popup span"),
   popupCloseBtn = document.querySelector(".popup button"),
-  idx = Math.floor(Math.random() * 21),
+  idx = Math.floor(Math.random() * 60),
   state = statesArray[idx],
   verbsMainInTable = [futureQ, futureA, futureN, presentQ, presentAFirst, presentN, pastQ, pastN],
   allVerbs = [...verbsMainInTable, presentAThird, pastA],
@@ -460,7 +460,15 @@ updateWord = () => {
 
   paintWord = () => {allVerbs.forEach(item => item.className = `clickable ${state.classes}`)}
   
-  let statePast;
+  let statePresentThird,
+    statePast;
+  switch (state.presentThird) {
+    case '':
+      statePresentThird = `${state.word}s`;
+      break;
+    default:
+      statePresentThird = `${state.presentThird}`;
+  }
   if (state.classes === 'red') {statePast = state.past}
   if (state.classes === '') {
     switch (lastSymbol) {
@@ -474,7 +482,7 @@ updateWord = () => {
 
   changeCurrentWord = () => {
     verbsMainInTable.forEach(item => item.textContent = state.word);
-    presentAThird.textContent = state.presentThird;
+    presentAThird.textContent = statePresentThird;
     pastA.textContent = statePast;
     paintWord();
     popupText.textContent = `To ${state.word} - ${state.translateRus}.`;
